f65e8e361b1b516d6681dadfd4ef869e2316aaea
Denis <dena.kuts1@gmail.com>
2016-05-31 21:30:10 +0300

    it can play video 100%

--- src/sample/CourseController.java
+++ src/sample/CourseController.java
@@ -4,9 +4,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class CourseController {
@@ -18,8 +24,27 @@ public class CourseController {
     private ListView<String> list;
 
     @FXML
+    private MediaView mediaPlayer;
+
+    private MediaPlayer player;
+
+    @FXML
     void openOnAction(ActionEvent event) {
 
+    }
+
+    void checkVideoExist(String path) {
+        File file = new File(path);
+        if (file.exists()) {
+            Media media = new Media(file.toURI().toString());
+            player = new MediaPlayer(media);
+            player.setAutoPlay(true);
+            mediaPlayer.setMediaPlayer(player);
+            player.pause();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Do you want to download video?", ButtonType.YES, ButtonType.NO);
+            alert.show();
+        }
     }
 
     @FXML
@@ -28,12 +53,39 @@ public class CourseController {
         assert list != null : "fx:id=\"list\" was not injected: check your FXML file 'CourseListForm.fxml'.";
         try {
             ArrayList<String> arrayList = Main.client.listOfCourses();
-            ObservableList<String> items = FXCollections.observableArrayList (arrayList);
+            ObservableList<String> items = FXCollections.observableArrayList(arrayList);
             list.getItems().addAll(items);
-        } catch (Exception w){
+        } catch (Exception w) {
 
         }
 
+
+
+    }
+
+    @FXML
+    void editOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void evaluateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void play(ActionEvent event){
+        player.play();
+    }
+
+    @FXML
+    void pause(ActionEvent event){
+        player.pause();
+    }
+
+    @FXML
+    void stop(ActionEvent event){
+        player.stop();
     }
 
 }
